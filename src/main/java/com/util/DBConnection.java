9268b0467ef8633ada53531b8018aa37543f3f12
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-10-11 16:49:16 +0530

    Update DBConnection.java

--- src/main/java/com/util/DBConnection.java
+++ src/main/java/com/util/DBConnection.java
@@ -1,34 +1,3 @@
-/*package com.util;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class DBConnection {
-	static Connection conn = null;
-
-	public static Connection getConnection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/bookberries", "root", "root");
-			if (conn != null){
-				return conn;
-			}else{
-				return conn;
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
-	public static void main(String[] args) {
-		getConnection();
-	}
-}
-*/
 package com.util;
 
 import java.sql.Connection;
@@ -42,7 +11,7 @@ public class DBConnection {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//Connection conn = DriverManager.getConnection("jdbc:mysql://127.12.83.130:3306/bookberries", "adminJdnpjzf","2TGE6WKRLTwh");
-			Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/bookberries","user", "password");
+			Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/sampledb","user", "password");
 			if (conn != null)
 				return conn;
 		} catch (SQLException e) {
