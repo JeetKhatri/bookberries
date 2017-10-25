e14f3f23e789d89f6a2fad08b33efb3628ebf628
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-10-25 19:07:50 +0530

    Update DBConnection.java

--- src/main/java/com/util/DBConnection.java
+++ src/main/java/com/util/DBConnection.java
@@ -11,7 +11,8 @@ public class DBConnection {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//Connection conn = DriverManager.getConnection("jdbc:mysql://127.12.83.130:3306/bookberries", "adminJdnpjzf","2TGE6WKRLTwh");
-			Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/sampledb","user", "password");
+			//Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/sampledb","user", "password");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://65.19.141.67:3306/sharvil_bookberries","sharvil_wp614","urvilsh@H1628");
 			if (conn != null)
 				return conn;
 		} catch (SQLException e) {
